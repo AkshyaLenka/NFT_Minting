ab8fbd342d3d9611d3d450a17514c679ee1c6d3f
Akshya Kumar Lenka <akshya.1792@gmail.com>
2022-07-28 12:08:49 +0530

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+    const MyNFT = await ethers.getContractFactory("MyNFT");
+  
+    // Start deployment, returning a promise that resolves to a contract object
+    const myNFT = await MyNFT.deploy();
+    console.log("Contract deployed to address:", myNFT.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+
+    //Address:0xaf8B8B0429Ebb3363041f7f1cddf9e7452B0a53a
